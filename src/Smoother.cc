745a142cb5928480759e161fe03979672cee2530
Thomas <thomas.stadelmayer@fau.de>
2015-06-02 17:04:09 +0200

    rückgängig

--- src/Smoother.cc
+++ src/Smoother.cc
@@ -1,18 +1,14 @@
+
 #include "Smoother.hh"
 #include "Types.hh"
 #include "Array.hh"
-#include "MGSolver.hh"
-#include <omp.h>
 
 void Smoother::smooth_red_black_gauss_seidel_2d ( Array & u,    // modify this array
-		Array & f,    // rhs
-		int times,     // number of sweeps
-		real h,       // spacing         
-		bool finest_grid
-		)
+                                                  Array & f,    // rhs
+                                                  int times,     // number of sweeps
+                                                  real h        // spacing         
+                                                )
 {
-
-	(void) finest_grid;
 
 	int width  = u.getSize(DIM_1D);
 	int height = u.getSize(DIM_2D);
@@ -24,32 +20,26 @@ void Smoother::smooth_red_black_gauss_seidel_2d ( Array & u,    // modify this a
 	for (int iter = 0; iter < times; iter++)
 	{
 		// red points
-#pragma omp parallel for
 		for (int j = 1; j < height-1; j++)
 		{
 			for (int i = 1; i < width-1; i++)
 			{
-				if(j == (height-1)*0.5 && i >= (width-1)*0.5) continue;
-                	        // inner domain
-                	        // i+j gerade
-               		        if( ((i + j) % 2) == 0){
-                        	        u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
-                        	}
+					// inner domain
+					// i+j gerade
+					if( ((i + j) % 2) == 0)
+						u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
 			}
 		}
 
 		// black points
-#pragma omp parallel for
 		for (int j = 1; j < height-1; j++)
 		{
 			for (int i = 1; i < width-1; i++)
 			{
-				if(j == (height-1)*0.5 && i >= (width-1)*0.5) continue;
-                	        // inner domain
-                	        // i+j ungerade
-               		        if( ((i + j) % 2) == 1){
-                        	        u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
-                        	}
+					// inner domain
+					// i+j ungerade
+					if( ((i + j) % 2) == 1)
+						u(i,j) = factor * (f(i,j) + h_2_inv * ( u(i-1, j) + u(i+1, j) + u(i, j+1) + u(i, j-1)));
 			}
 		}
 	}
